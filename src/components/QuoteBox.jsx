3e9cd659d8a97757a0b782c5a7a5e07048bb4d4b
Mohanad Farid <106895779+Mohanadfarid@users.noreply.github.com>
2023-12-24 07:30:54 +0200

    added the user storys from free code camp

---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function QuoteBox() {
+  return (
+    <div id="quote-box">
+      <p id="text"></p>
+      <p id="author"></p>
+      <button id="new-quote">New quote</button>
+      <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">icon place holder</a>
+
+    </div>
+  );
+}
