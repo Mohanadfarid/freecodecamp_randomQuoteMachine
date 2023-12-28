390be5e2d57c568fc7b08d78e66c5a698f7a7680
Mohanad Farid <106895779+Mohanadfarid@users.noreply.github.com>
2023-12-28 09:29:11 +0200

    finished styleing the quote component

--- src/components/QuoteBox/QuoteBox.jsx
+++ src/components/QuoteBox/QuoteBox.jsx
@@ -1,14 +1,30 @@
 import React from "react";
+import { FaTwitter, FaTumblr } from "react-icons/fa";
 import "./quoteBox.scss";
-export default function QuoteBox({ info ,geranteQuote}) {
+export default function QuoteBox({ info, geranteQuote }) {
   return (
     <div id="quote-box" className="quote-box">
-      <p id="text" className="text">{info.quote}</p>
-      <p id="author" className="author">- {info.author}</p>
-      <button id="new-quote" onClick={geranteQuote}>New quote</button>
-      <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">
-        icon place holder
-      </a>
+      <p id="text" className="text">
+        {info.quote}
+      </p>
+      <p id="author" className="author">
+        - {info.author}
+      </p>
+      <button id="new-quote" className="new-quote" onClick={geranteQuote}>
+        New quote
+      </button>
+      <div className="icons-container">
+        <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">
+          <div className="icon-container">
+            <FaTwitter className="icon" />
+          </div>
+        </a>
+        <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">
+          <div className="icon-container">
+            <FaTumblr className="icon" />
+          </div>
+        </a>
+      </div>
     </div>
   );
 }
