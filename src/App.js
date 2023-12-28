37134ba9eecd66b2c549002012c726262c597360
Mohanad Farid <106895779+Mohanadfarid@users.noreply.github.com>
2023-12-28 09:40:28 +0200

    added the dynamic coloring to the quotes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,40 @@ import { useEffect, useState } from "react";
 import { fetchQuote } from "./api";
 import QuoteBox from "./components/QuoteBox/QuoteBox.jsx";
 
+function generateRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 function App() {
   const handleNewQuote = async () => {
     setquoteInfo(await fetchQuote());
+    handleChangingRandomCorlor();
+  };
+
+  const handleChangingRandomCorlor = () => {
+    const newColor = generateRandomColor();
+    setRandomColor(newColor);
+    document.body.style.backgroundColor = newColor; // Set body background color
   };
 
   useEffect(() => {
     handleNewQuote();
   }, []);
 
+  const [randomColor, setRandomColor] = useState(generateRandomColor());
   const [quoteInfo, setquoteInfo] = useState({
     qoute: "",
     author: "",
     category: "",
   });
-  return <QuoteBox info={quoteInfo} geranteQuote={handleNewQuote}/>;
+
+  return (
+    <QuoteBox
+      randomColor={randomColor}
+      info={quoteInfo}
+      geranteQuote={handleNewQuote}
+    />
+  );
 }
 
 export default App;
--- src/components/QuoteBox/QuoteBox.jsx
+++ src/components/QuoteBox/QuoteBox.jsx
@@ -1,26 +1,37 @@
 import React from "react";
 import { FaTwitter, FaTumblr } from "react-icons/fa";
 import "./quoteBox.scss";
-export default function QuoteBox({ info, geranteQuote }) {
+export default function QuoteBox({ info, geranteQuote, randomColor }) {
   return (
     <div id="quote-box" className="quote-box">
-      <p id="text" className="text">
+      <p id="text" className="text" style={{ color: randomColor }}>
         {info.quote}
       </p>
-      <p id="author" className="author">
+      <p id="author" className="author" style={{ color: randomColor }}>
         - {info.author}
       </p>
-      <button id="new-quote" className="new-quote" onClick={geranteQuote}>
+      <button
+        id="new-quote"
+        className="new-quote"
+        onClick={geranteQuote}
+        style={{ backgroundColor: randomColor }}
+      >
         New quote
       </button>
       <div className="icons-container">
         <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">
-          <div className="icon-container">
+          <div
+            className="icon-container"
+            style={{ backgroundColor: randomColor }}
+          >
             <FaTwitter className="icon" />
           </div>
         </a>
         <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">
-          <div className="icon-container">
+          <div
+            className="icon-container"
+            style={{ backgroundColor: randomColor }}
+          >
             <FaTumblr className="icon" />
           </div>
         </a>
